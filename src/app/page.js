ef0f7fe1fb939b293aa45247241c5ed9d7c471a8
Tavneet Singh <tavneet170@gmail.com>
2026-01-05 09:25:09 +0530

    src/app/page.js update a code

--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,32 @@
+import AboutData from '@/HomePageComponents/AboutData';
+import Footer from '@/HomePageComponents/Footer';
+import FrontImage from '@/HomePageComponents/FrontImage';
+import NavBar from '@/HomePageComponents/NavBar';
+import Process from '@/HomePageComponents/Process';
+import WhyChooseWe from '@/HomePageComponents/WhyChooseWe';
+
 export default function Home() {
+
   return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>Site is Live!</h1>
-      <p>Agar ye dikh raha hai, toh routing sahi hai. Error niche wale components mein hai.</p>
-    </div>
+    <>
+      <div className="">
+        <section>
+          <NavBar />
+        </section>
+        <section className=''>
+          <FrontImage />
+        </section>
+        <section className='mt-16 md:mt-10  mb-12'>
+          <WhyChooseWe />
+        </section>
+        <section className='mt-11 md:mt-12  mb-12'>
+          <Process />
+        </section>
+        <section className='mt-10'>
+          <AboutData />
+        </section>
+        <Footer/>
+      </div>
+    </>
   );
 }
-
-
-// import AboutData from '@/HomePageComponents/AboutData';
-// import Footer from '@/HomePageComponents/Footer';
-// import FrontImage from '@/HomePageComponents/FrontImage';
-// import NavBar from '@/HomePageComponents/NavBar';
-// import Process from '@/HomePageComponents/Process';
-// import WhyChooseWe from '@/HomePageComponents/WhyChooseWe';
-
-// export default function Home() {
-
-//   return (
-//     <>
-//       <div className="">
-//         <section>
-//           <NavBar />
-//         </section>
-//         <section className=''>
-//           <FrontImage />
-//         </section>
-//         <section className='mt-16 md:mt-10  mb-12'>
-//           <WhyChooseWe />
-//         </section>
-//         <section className='mt-11 md:mt-12  mb-12'>
-//           <Process />
-//         </section>
-//         <section className='mt-10'>
-//           <AboutData />
-//         </section>
-//         <Footer/>
-//       </div>
-//     </>
-//   );
-// }
